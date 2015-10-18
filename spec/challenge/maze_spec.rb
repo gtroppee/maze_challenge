e54499ce00977584557e4017b73e76dae00157db
gtroppee <gtroppee@gmail.com>
2015-10-18 13:01:12 +0200

    add spec

--- spec/challenge/maze_spec.rb
+++ spec/challenge/maze_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Maze do
 
   context 'when the source and the target are equal' do
     it '#initialize' do
+      expect { Maze.new(2, 4) }.not_to raise_exception
       expect { Maze.new(2, 2) }.to raise_exception(ArgumentError, "The source and the target must be different.")
     end
   end
